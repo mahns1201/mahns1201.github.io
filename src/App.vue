28a133816b331e8bdac8d168419f1a71140d5ed1
서민혁 <mahns1201@gmail.com>
2022-03-21 18:31:00 +0900

    [feat] .adjustment

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="adjustment">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
@@ -15,14 +16,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import '@/styles/style.scss';
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '@/styles/main.scss';
 </style>

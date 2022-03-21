44f5d4497ba1cd96a4bdf73c0c2a62a8ef2f28c8
서민혁 <mahns1201@gmail.com>
2022-03-21 18:03:49 +0900

    [feat] sass, sass-loader

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   outputDir: './docs',
   publicPath: '',
+
+  css: {
+    loaderOptions: {
+      scss: {
+        data: `
+            @import "@/styles/style.scss";
+            `,
+      },
+    },
+  },
 };

0610d857b07f24f7f8b1520c35062891a8a8ddd7
서민혁 <mahns1201@gmail.com>
2022-03-22 16:13:06 +0900

    [fix] directory

---
+++ src/views/sections/TabSection.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="tab">
+    <select name="tab" id="tab">
+      <option value="skills">Skills</option>
+      <option value="works">Works</option>
+    </select>
+  </nav>
+</template>

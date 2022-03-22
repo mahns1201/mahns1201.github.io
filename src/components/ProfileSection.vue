54ade15236a46fbd2a8dac6c9fa2ff34134c0356
서민혁 <mahns1201@gmail.com>
2022-03-22 14:00:27 +0900

    [feat] profile-section

---
+++ src/components/ProfileSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="profile">
+    <img src="https://source.unsplash.com/random/250x250" alt="avatar" />
+    <div>
+      <div class="text-lg">Mahns</div>
+      <div>Developer</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>

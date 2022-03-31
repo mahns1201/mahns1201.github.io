0ebfb6427833304aa57319c6b8867eeed278612b
서민혁 <mahns1201@gmail.com>
2022-04-01 00:59:49 +0900

    [style] profile-banner

--- src/views/sections/ProfileSection.vue
+++ src/views/sections/ProfileSection.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="profile">
+    <div class="profile__banner">
+      <div class="blur"></div>
+    </div>
     <img src="https://source.unsplash.com/random/250x250" alt="avatar" />
     <div>
       <div>Mahns</div>

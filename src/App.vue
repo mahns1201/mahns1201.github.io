44dca48d05851a230c33ccc22f15bf412f452903
서민혁 <mahns1201@gmail.com>
2022-04-01 00:09:18 +0900

    [feat] footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="adjustment">
+  <div class="adjustment adjustment__height">
     <index-page />
+  </div>
+  <div>
+    <footer-section />
   </div>
 </template>
 
 <script>
 import IndexPage from '@/views/IndexPage';
+import FooterSection from '@/views/sections/FooterSection.vue';
 
 export default {
   name: 'App',
 
   components: {
     IndexPage,
+    FooterSection,
   },
 };
 </script>
---
+++ src/views/sections/FooterSection.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="footer">
+    <div class="footer__text">
+      © {{ new Date().getFullYear() }}. mahns. All right reserved.
+    </div>
+  </section>
+</template>

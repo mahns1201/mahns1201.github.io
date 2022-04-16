d139fa126c3d16af27fa7addb5b25ede3adf2236
mahns <mahns1201@gmail.com>
2022-04-16 23:05:28 +0900

    [chore] init

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,11 @@
 <template>
-  <div>
-    <div>
-      <profile-section />
-    </div>
-    <div class="adjustment adjustment__height">
-      <router-view />
-    </div>
-    <div>
-      <footer-section></footer-section>
-    </div>
-  </div>
+  <div>App</div>
 </template>
 
 <script>
-import ProfileSection from '@/views/sections/ProfileSection.vue';
-import FooterSection from '@/views/sections/FooterSection.vue';
-
 export default {
   name: 'App',
-
-  components: {
-    ProfileSection,
-    FooterSection,
-  },
 };
 </script>
 
-<style lang="scss">
-@import '@/styles/main.scss';
-</style>
+<style lang="scss"></style>

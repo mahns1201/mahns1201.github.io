43fa0bbb784edd272f77939498079cea20f67a17
mahns <mahns1201@gmail.com>
2022-04-17 21:25:38 +0900

    [init] main-component; carousel

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>App</div>
+  <!-- 라우터 대신 캐러셀이 들어간다. -->
+  <!-- 라우터 안 쓰는 것으로 결정되면 npm uninstall 하자. -->
+  <div>
+    <main-carousel />
+  </div>
 </template>
 
 <script>
+import MainCarousel from '@/components/carousel/MainCarousel.vue';
+
 export default {
   name: 'App',
+
+  components: { MainCarousel },
 };
 </script>
 
---
+++ src/components/carousel/MainCarousel.vue
@@ -0,0 +1,7 @@
+<template>Carousel</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>

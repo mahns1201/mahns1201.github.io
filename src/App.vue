510842ea0e6d96946c2dd36b1a2beb80fb7290a5
서민혁 <mahns1201@gmail.com>
2022-05-02 18:10:14 +0900

    [init]

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,48 @@
 <template>
   <!-- 라우터 대신 캐러셀이 들어간다. -->
   <!-- 라우터 안 쓰는 것으로 결정되면 npm uninstall 하자. -->
+  <div class="test">
+    <div class="layout">
+      <div class="page">page1</div>
+      <div class="page">page2</div>
+      <div class="page">page3</div>
+    </div>
+  </div>
+
   <div>
-    <main-carousel class="carousel" />
+    <button @click="click">1</button>
+    <button @click="click">2</button>
+    <button @click="click">3</button>
   </div>
 </template>
 
 <script>
-import MainCarousel from '@/components/carousel/MainCarousel.vue';
-
 export default {
   name: 'App',
 
-  components: { MainCarousel },
+  methods: {
+    click() {
+      console.log(this.$refs);
+    },
+  },
 };
 </script>
 
 <style lang="scss">
 @import '@/styles/main';
+
+.test {
+  overflow: hidden;
+  .layout {
+    display: flex;
+    overflow: hidden;
+    width: 300vw;
+    height: 100%;
+
+    .page {
+      width: 100vw;
+      float: left;
+    }
+  }
+}
 </style>

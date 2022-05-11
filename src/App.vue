a8d5544febbf4605d0725cec95eac7ae9e920a85
mahns <mahns1201@gmail.com>
2022-05-12 01:11:44 +0900

    [style] profile section

--- src/App.vue
+++ src/App.vue
@@ -1,115 +1,30 @@
 <template>
-  <div>
-    <section class="profile">
-      <div class="profile__container">
-        <img src="https://source.unsplash.com/random/250x250" alt="profile" />
-      </div>
-      <div class="profile__description">
-        <h1>서민혁</h1>
-        <h2>팬을 잡는 개발자</h2>
-        <p>프론트앤드</p>
-        <p>
-          <span>Github</span>
-          <a href="https://github.com/mahns1201"
-            ><span>https://github.com/mahns1201</span></a
-          >
-        </p>
-        <p>
-          <span>Blog</span>
-          <a href="https://velog.io/@mahns"
-            ><span>https://velog.io/@mahns</span></a
-          >
-        </p>
-        <p><span>Email</span><span>mahns1201@gmail.com</span></p>
-      </div>
-    </section>
+  <main class="adjustment">
+    <profile-section />
 
     <section>
       <h2>Skills</h2>
-
-      <!-- basic -->
-      <div>
-        <img
-          src="https://img.shields.io/badge/-HTML5-red?style=flat&logo=html5&logoColor=ffffff"
-          alt="HTML5"
-          class="img__tags"
-        />
-        <img
-          src="https://img.shields.io/badge/-CSS3-blue?style=flat&logo=css3"
-          alt="CSS3"
-          class="img__tags"
-        />
-        <img
-          src="https://img.shields.io/badge/-JavaScript-yellow?style=flat&logo=javascript"
-          alt="JavaScript"
-          class="img__tags"
-        />
-      </div>
-
-      <!-- front-end -->
-      <div>
-        <img
-          src="https://img.shields.io/badge/-SCSS-ff69b4?style=flat&logo=sass&logoColor=ffffff"
-          alt="SCSS"
-          class="img__tags"
-        />
-        <img
-          src="https://img.shields.io/badge/-Tailwind-blue?style=flat&logo=TailwindCSS"
-          alt="Tailwind"
-          class="img__tags"
-        />
-
-        <img
-          src="https://img.shields.io/badge/-Vue.js-green?style=flat&logo=vue.js"
-          alt="Vue"
-          class="img__tags"
-        />
-      </div>
-
-      <!-- back-end -->
-      <div>
-        <img
-          src="https://img.shields.io/badge/-Java-white?style=flat&logo=java&logoColor=ff0000"
-          alt="Java"
-          class="img__tags"
-        />
-        <img
-          src="https://img.shields.io/badge/-Spring-green?style=flat&logo=spring&logoColor=ffffff"
-          alt="Spring"
-          class="img__tags"
-        />
-        <img
-          src="https://img.shields.io/badge/-Node.js-green?style=flat&logo=node.js&logoColor=ffffff"
-          alt="Node"
-          class="img__tags"
-        />
-        <img
-          src="https://img.shields.io/badge/-Serverless-red?style=flat&logo=serverless"
-          alt="Serverless"
-          class="img__tags"
-        />
-      </div>
-
-      <!-- VCS -->
-      <div>
-        <img
-          src="https://img.shields.io/badge/-Git-white?style=flat&logo=git"
-          alt="Git"
-          class="img__tags"
-        />
-      </div>
     </section>
 
     <section>
-      <h2>Projects</h2>
+      <h2>Work Experiences</h2>
     </section>
-    <section>Awards</section>
-  </div>
+
+    <section>
+      <h2>Awards</h2>
+    </section>
+  </main>
 </template>
 
 <script>
+import ProfileSection from '@/components/section/ProfileSection';
+
 export default {
   name: 'App',
+
+  components: {
+    ProfileSection,
+  },
 
   methods: {
     click() {
---
+++ src/components/section/ProfileSection.vue
@@ -0,0 +1,120 @@
+<template>
+  <section class="profile">
+    <div class="profile__container">
+      <img src="https://source.unsplash.com/random/250x250" alt="profile" />
+    </div>
+    <div class="profile__description">
+      <div class="devide">
+        <h4>Contact</h4>
+
+        <div>
+          <label class="label">Name</label><span>서민혁, 팬을 잡는 개발자</span>
+        </div>
+        <div><label class="label">Phone</label><span>010-5235-2562</span></div>
+        <div>
+          <label class="label">Email</label><span>mahns1201@gmail.com</span>
+        </div>
+      </div>
+
+      <div class="devide">
+        <h4>Chanel</h4>
+
+        <div>
+          <label class="label">Github</label>
+          <a href="https://github.com/mahns1201"
+            ><span>https://github.com/mahns1201</span></a
+          >
+        </div>
+        <div>
+          <label class="label">Blog</label>
+          <a href="https://velog.io/@mahns"
+            ><span>https://velog.io/@mahns</span></a
+          >
+        </div>
+        <div>
+          <label class="label">Email</label><span>mahns1201@gmail.com</span>
+        </div>
+      </div>
+
+      <div class="devide">
+        <h4>Skills</h4>
+
+        <!-- basic -->
+        <div>
+          <h5>FRONT-END</h5>
+          <img
+            src="https://img.shields.io/badge/-HTML5-red?style=flat&logo=html5&logoColor=ffffff"
+            alt="HTML5"
+            class="img__tags"
+          />
+          <img
+            src="https://img.shields.io/badge/-CSS3-blue?style=flat&logo=css3"
+            alt="CSS3"
+            class="img__tags"
+          />
+          <img
+            src="https://img.shields.io/badge/-JavaScript-yellow?style=flat&logo=javascript"
+            alt="JavaScript"
+            class="img__tags"
+          />
+          <img
+            src="https://img.shields.io/badge/-SCSS-ff69b4?style=flat&logo=sass&logoColor=ffffff"
+            alt="SCSS"
+            class="img__tags"
+          />
+          <img
+            src="https://img.shields.io/badge/-Tailwind-blue?style=flat&logo=TailwindCSS"
+            alt="Tailwind"
+            class="img__tags"
+          />
+
+          <img
+            src="https://img.shields.io/badge/-Vue.js-green?style=flat&logo=vue.js"
+            alt="Vue"
+            class="img__tags"
+          />
+        </div>
+
+        <div>
+          <h5>BACK-END</h5>
+          <img
+            src="https://img.shields.io/badge/-Java-white?style=flat&logo=java&logoColor=ff0000"
+            alt="Java"
+            class="img__tags"
+          />
+          <img
+            src="https://img.shields.io/badge/-Spring-green?style=flat&logo=spring&logoColor=ffffff"
+            alt="Spring"
+            class="img__tags"
+          />
+          <img
+            src="https://img.shields.io/badge/-Node.js-green?style=flat&logo=node.js&logoColor=ffffff"
+            alt="Node"
+            class="img__tags"
+          />
+          <img
+            src="https://img.shields.io/badge/-Serverless-red?style=flat&logo=serverless"
+            alt="Serverless"
+            class="img__tags"
+          />
+        </div>
+
+        <!-- VCS -->
+        <div>
+          <h5>VSC</h5>
+          <img
+            src="https://img.shields.io/badge/-Git-white?style=flat&logo=git"
+            alt="Git"
+            class="img__tags"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
